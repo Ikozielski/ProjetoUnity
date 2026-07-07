62f45c707f56cb3ab290dc1defc618a04d61fe9a
Iury Kozielski <meidiaenove@gmail.com>
2026-07-07 00:11:03 -0300

    Adicionando movimento na raquete

--- Assets/Scripts/RaqueteController.cs
+++ Assets/Scripts/RaqueteController.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class RaqueteController : MonoBehaviour
 {
+    public float velocidadeY = 10f;
+
+    Vector3 posicao;
+    float limiteY;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float alturaCamera = Camera.main.orthographicSize;
+        float metadeRaquete = GetComponent<SpriteRenderer>().bounds.extents.y;
+        limiteY = alturaCamera - metadeRaquete;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        posicao = transform.position;
+        posicao.y += Input.GetAxisRaw("Vertical") * velocidadeY * Time.deltaTime;
+        posicao.y = Mathf.Clamp(posicao.y, -limiteY, limiteY);
+        transform.position = posicao;
     }
 }
